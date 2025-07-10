f8590db05c9d2d0c05fe1ace898fc4fedaf3ccd8
Roshanyadav18 <roshan.opsstation@gmail.com>
2025-07-10 11:49:45 +0530

    upadte1

--- example/example.tf
+++ example/example.tf
@@ -2,6 +2,7 @@ module "labels" {
   source      = "./../"
   name        = "app"
   environment = "test"
+  managedby  = "shanav-tech"
   label_order = ["name", "environment"]
   attributes  = ["private"]
   extra_tags = {
